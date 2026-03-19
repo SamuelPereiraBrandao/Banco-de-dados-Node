1d1a5eaac859d4d88f1c2e37fece76e2f20b647c
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2026-03-19 10:26:09 -0300

    server ts

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,10 @@
 import { Knex, knex as setupKnex } from 'knex'
-import { env } from './env'
 
 export const config: Knex.Config = {
-  client: env.DATABASE_CLIENT,
-  connection:
-    env.DATABASE_CLIENT === 'sqlite'
-      ? {
-          filename: env.DATABASE_URL,
-        }
-      : env.DATABASE_URL,
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import { app } from './app'
-import { env } from './env'
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app
   .listen({
-    port: env.PORT,
+    port: 3333,
   })
   .then(() => {
     console.log('HTTP Server Running!')
